43381950a7b08cee62ad1a800a88ad0669ea16d8
Bjoern Mueller <shindara09@gmail.com>
2020-08-04 01:11:35 +0100

    yapf

--- personfinder.py
+++ personfinder.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """
 This tool is created to measure the performance of a dataset in detecting persons using a Raspberry Pi with Coral Edge TPU.
 Special thanks to https://www.pyimagesearch.com/ for inspiration and the imutils library, more documentation and explaination can be found on their homepage.
@@ -26,36 +25,60 @@ class GPIOHelper():
         self.yellow_pin = 8
         self.green_pin = 10
 
-    def light(self, pin, on = False):
+    def light(self, pin, on=False):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, on)
 
     def green_on(self):
-        self.light(self.green_pin, on = True)
+        self.light(self.green_pin, on=True)
         self.light(self.yellow_pin)
 
     def yellow_on(self):
         self.light(self.yellow_pin)
-        self.light(self.yellow_pin, on = True)
+        self.light(self.yellow_pin, on=True)
 
     def all_off(self):
         self.light(self.green_pin)
         self.light(self.yellow_pin)
 
+
 gpio_helper = GPIOHelper()
+
 
 def parse_arguments():
     ap = argparse.ArgumentParser()
-    ap.add_argument("-m", "--model", default="mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite",
-                    help="path to TensorFlow Lite object detection model")
-    ap.add_argument("-l", "--labels", default="coco_labels.txt",
-                    help="path to labels file")
-    ap.add_argument("-c", "--confidence", type=float, default=0.3,
-                    help="minimum probability to filter weak detections")
-    ap.add_argument("-o", "--objects", default={0}, help="objects needed, default is 0 (person)")
-    ap.add_argument("-d", "--display", default=True, help="if false, there will be no display output")
-    ap.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    ap.add_argument(
+        "-m",
+        "--model",
+        default="mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite",
+        help="path to TensorFlow Lite object detection model")
+    ap.add_argument(
+        "-l",
+        "--labels",
+        default="coco_labels.txt",
+        help="path to labels file")
+    ap.add_argument(
+        "-c",
+        "--confidence",
+        type=float,
+        default=0.3,
+        help="minimum probability to filter weak detections")
+    ap.add_argument(
+        "-o",
+        "--objects",
+        default={0},
+        help="objects needed, default is 0 (person)")
+    ap.add_argument(
+        "-d",
+        "--display",
+        default=True,
+        help="if false, there will be no display output")
+    ap.add_argument(
+        "-v",
+        "--verbose",
+        help="increase output verbosity",
+        action="store_true")
     return vars(ap.parse_args())
 
 
@@ -78,20 +101,20 @@ def person_detected_led_interface(results):
 def show_on_screen(frame_id, label, r, rotated, starting_time, scale):
     # extract the bounding box and box and predicted class label
     raw_box = r.bounding_box
-    raw_box = raw_box/scale
+    raw_box = raw_box / scale
     box = raw_box.flatten().astype("int")
     (startX, startY, endX, endY) = box
     # draw the bounding box and label on the image
     cv2.rectangle(rotated, (startX, startY), (endX, endY), (0, 255, 0), 2)
     y = startY - 15 if startY - 15 > 15 else startY + 15
     text = "{}: {:.2f}%".format(label, r.score * 100)
-    cv2.putText(rotated, text, (startX, y),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+    cv2.putText(rotated, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (0, 255, 0), 1)
     elapsed_time = time.time() - starting_time
     fps = frame_id / elapsed_time
     fps_text = "FPS: {:.2f}".format(fps)
-    cv2.putText(rotated, fps_text, (10, 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+    cv2.putText(rotated, fps_text, (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (0, 255, 255), 2)
 
 
 def main():
@@ -132,12 +155,12 @@ def main():
         frame = cv2.rotate(frame, cv2.ROTATE_180)
         orig = frame.copy()
         orig_width = orig.shape[1]
-        print("originale Breite war mal {} ---",format(orig_width))
+        print("originale Breite war mal {} ---", format(orig_width))
 
         # skalierung:
 
         frame = imutils.resize(frame, width=300)
-        scale = 300/orig_width
+        scale = 300 / orig_width
 
         # prepare the frame for object detection by converting (1) it
         # from BGR to RGB channel ordering and then (2) from a NumPy
@@ -146,16 +169,20 @@ def main():
         frame = Image.fromarray(frame)
         # make predictions on the input frame
         start = time.time()
-        results = model.detect_with_image(frame, threshold=args.get("confidence"),
-                                          # Schwellwert ab dem Objekte als erkannt gelten
-                                          keep_aspect_ratio=True, relative_coord=False)
+        results = model.detect_with_image(
+            frame,
+            threshold=args.get("confidence"),
+            # Schwellwert ab dem Objekte als erkannt gelten
+            keep_aspect_ratio=True,
+            relative_coord=False)
         # results is a list of DetectionCandidate(label_id, score, x1, y1, x2, y2)
         end = time.time()
-        log.info("DETECTION TIME is {:.6} s".format(end-start))
+        log.info("DETECTION TIME is {:.6} s".format(end - start))
         person_detected_led_interface(results)
 
         for r in results:
-            label = labels[r.label_id]  # fits label from imported labels to the result
+            label = labels[
+                r.label_id]  # fits label from imported labels to the result
             if args.get("display"):
                 show_on_screen(frame_id, label, r, orig, starting_time, scale)
             else:
